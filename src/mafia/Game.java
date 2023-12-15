7fd2c89dc76eb4dd3c74207bf05df5b306685cfa
BuildingOwner <wodhks0911@gmail.com>
2023-12-15 19:17:20 +0900

    코드 통합 및 버그 수정

--- src/mafia/Game.java
+++ src/mafia/Game.java
@@ -5,8 +5,8 @@ import java.util.*;
 
 public class Game extends Thread {
   private ArrayList<ServerThread> serverThreads;
-  private String[] jobs = {"시민", "시민2","마피아","의사","경찰"}; //역할 추가 H
-  private int[] jobCount = {1, 1, 1, 1, 1}; //5개 H
+  private String[] jobs = {"시민", "마피아", "의사", "경찰"}; //역할 추가 H
+  private int[] jobCount = {1, 1, 1, 1}; //5개 H
   private List<User> users = new ArrayList<>();
   DayNight dayNight = DayNight.HEAL;
   private Boolean gameFlag = true;
@@ -26,11 +26,11 @@ public class Game extends Thread {
     }
     if (skill.equals("kill")) {
       mafiaPick = pick;
-      noticePersonal(s, "당신은 " + mafiaPick + "님을 죽일겁니다.", "chat");
+      noticePersonal(s, "개인 : 당신은 " + mafiaPick + "님을 죽일겁니다.", "chat");
     }
     if (skill.equals("heal")) {
       doctorPick = pick;
-      noticePersonal(s, "당신은 " + doctorPick + "님을 살릴겁니다.", "chat");
+      noticePersonal(s, "개인 : 당신은 " + doctorPick + "님을 살릴겁니다.", "chat");
     }
 
   }
@@ -41,10 +41,10 @@ public class Game extends Thread {
     } else {
       for (int i = 0; i < users.size(); i++) {
         if (mafiaPick.equals(users.get(i).name.split("#")[0])) {
-          users.get(i).isDead = true;
           notice("간밤에 " + users.get(i).name + "님이 마피아에게 습격당해 죽었습니다.", "chat");
-          serverThreads.get(i).isDead = true;
           noticePersonal(serverThreads.get(i), "", "death");
+          users.get(i).isDead = true;
+          serverThreads.get(i).isDead = true;
         }
       }
     }
@@ -107,9 +107,10 @@ public class Game extends Thread {
         index = i;
       }
     }
-    users.get(index).isDead = true;
+
     notice(users.get(index).name + "님이 투표로 죽었습니다.", "chat");
     notice(users.get(index).name + "님은 " + users.get(index).jab + "입니다.", "chat");
+    users.get(index).isDead = true;
     serverThreads.get(index).isDead = true;
     noticePersonal(serverThreads.get(index), "", "death");
 
@@ -122,9 +123,9 @@ public class Game extends Thread {
   public void init() {
     Random rand = new Random();
     for (int i = 0; i < serverThreads.size(); i++) {
-      int num = 0;
+      int num = rand.nextInt(jobs.length);;
       while (jobCount[num] <= 0) {
-        num = rand.nextInt(5); //5로 변경 H
+        num = rand.nextInt(jobs.length); // 이제 변경 안해도 됨
       }
       jobCount[num]--;
       User u = new User(serverThreads.get(i).name, jobs[num]);
@@ -135,7 +136,7 @@ public class Game extends Thread {
 
     voteName = new String[serverThreads.size()];
     voteCount = new int[serverThreads.size()];
-    for(int i=0; i<voteName.length; i++){
+    for (int i = 0; i < voteName.length; i++) {
       voteName[i] = "";
       voteCount[i] = 0;
     }
@@ -145,12 +146,7 @@ public class Game extends Thread {
     timer();
     notice("", "member");
     notice("true", "isGameRun");
-    while (gameFlag) {
-      checkFinish();
-      if (dayNight == DayNight.DAY) {
-
-      }
-    }
+    checkFinish();
   }
 
   private void timer() {
@@ -158,64 +154,81 @@ public class Game extends Thread {
     timer.schedule(new TimerTask() {
       @Override
       public void run() {
-        switch (dayNight) {
-          case DAY:
-            dayNight = DayNight.VOTE;
-            notice("now Vote", "chat");
-            notice("vote", "dayNight");
-            break;
-          case VOTE:
-            voteKill();
-            dayNight = DayNight.NIGHT;
-            notice("now Night", "chat");
-            notice("night", "dayNight");
-            break;
-          case NIGHT:
-            dayNight = DayNight.HEAL;
-            notice("now Heal", "chat");
-            notice("heal", "dayNight");
-            break;
-          case HEAL:
-            killProcessing();
-            dayNight = DayNight.DAY;
-            notice("now Day", "chat");
-            notice("day", "dayNight");
-            break;
+        if (gameFlag) {
+          switch (dayNight) {
+            case DAY:
+              dayNight = DayNight.VOTE;
+              notice("now Vote", "chat");
+              notice("vote", "dayNight");
+              break;
+            case VOTE:
+              voteKill();
+              if (!checkFinish()) {
+                dayNight = DayNight.NIGHT;
+                notice("now Night", "chat");
+                notice("night", "dayNight");
+              }
+              break;
+            case NIGHT:
+              dayNight = DayNight.HEAL;
+              notice("now Heal", "chat");
+              notice("heal", "dayNight");
+              break;
+            case HEAL:
+              killProcessing();
+              if (!checkFinish()) {
+                dayNight = DayNight.DAY;
+                notice("now Day", "chat");
+                notice("day", "dayNight");
+              }
+              break;
+          }
         }
       }
     }, 0, 20000);
+
 
     final int[] leftTime = {20};
     timer.schedule(new TimerTask() {
       @Override
       public void run() {
-        notice(String.valueOf(leftTime[0]--), "leftTime");
-        if (leftTime[0] <= 0) {
-          leftTime[0] = 20;
+        if (gameFlag) {
+          notice(String.valueOf(leftTime[0]--), "leftTime");
+          if (leftTime[0] <= 0) {
+            leftTime[0] = 20;
+          }
         }
       }
     }, 0, 1000);
+
   }
 
-  private void checkFinish() {
+  private Boolean checkFinish() {
     int citizen = 0;
     int enemy = 0;
     for (User u : users) {
-      if (u.jab.equals("시민") || u.jab.equals("의사")) {
-        citizen++;
-      } else {
-        enemy++;
+      if (!u.isDead) {
+        if (u.jab.equals("마피아")) {
+          enemy++;
+        } else {
+          citizen++;
+        }
       }
     }
+
     if (enemy == 0) {
       gameFlag = false;
       notice("citizen win", "chat");
       notice("false", "isGameRun");
+      return true;
     } else if (citizen <= enemy) {
       gameFlag = false;
       notice("enemy win", "chat");
       notice("false", "isGameRun");
+      return true;
     }
+
+    return false;
   }
 
   public void notice(String str, String method) {
