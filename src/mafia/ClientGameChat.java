1ae2c920049f2a892faca18179e70a3076dfdea0
juwonhee <jwonhee398@gmail.com>
2023-12-17 22:03:01 +0900

    StartPanel, GamePanel, ClientGameChat 코드 정리 및 주석

--- src/mafia/ClientGameChat.java
+++ src/mafia/ClientGameChat.java
@@ -40,33 +40,29 @@ public class ClientGameChat extends Thread {
           switch (msg[1]) {
             case "day":
               clientGame.now = DayNight.DAY;
-              clientGame.gp.ment.setText("<html>시민 여러분, 지금은 낮입니다.<br>의심과 추론을 통해 진실을 밝혀내는 시간입니다.<br>채팅을 통해 대화할 수 있습니다.</html>");
-              //clientGame.gp.setBackground(Color.DARK_GRAY);
-              displayRoleImage("src/mafia/image/토론활동.png");
+              //GamePanel화면에 ment JLabel에 출력
+              clientGame.gp.ment.setText("<html>시민 여러분, 지금은 낮입니다.<br>의심과 추론을 통해 진실을 밝혀내는 시간입니다.<br>채팅을 통해 대화할 수 있습니다.</html>"); //낮임을 알리는 멘트 출력
+              displayRoleImage("src/mafia/image/토론활동.png"); //토론하는 이미지 출력
               break;
             case "vote":
               clientGame.now = DayNight.VOTE;
-              clientGame.gp.ment.setText("<html>모든 플레이어들, 지금은 투표 시간입니다.<br>누구에게 의심의 눈길을 보낼까요?<br>투표 창에 지목할 사람의 이름을 입력하세요. 최다 투표 득점자는 처형!</html>");
-              //clientGame.gp.setBackground(Color.DARK_GRAY);
-              displayRoleImage("src/mafia/image/투표활동.png");
+              clientGame.gp.ment.setText("<html>모든 플레이어들, 지금은 투표 시간입니다.<br>누구에게 의심의 눈길을 보낼까요?<br>투표 창에 지목할 사람의 이름을 입력하세요. 최다 투표 득점자는 처형!</html>"); //투표 시간임을 알리는 멘트 출력
+              displayRoleImage("src/mafia/image/투표활동.png"); //투표하는 이미지 출력
               break;
             case "night":
               clientGame.now = DayNight.NIGHT;
-              clientGame.gp.ment.setText("<html>어둠이 내려오고, 밤이 시작됩니다.<br>마피아와 다른 역할들이 행동할 시간입니다.<br>투표 창에 지목할 사람의 이름을 입력하세요.</html>");
-              //clientGame.gp.setBackground(Color.BLACK);
-              displayRoleImage("src/mafia/image/마피아활동.png");
+              clientGame.gp.ment.setText("<html>어둠이 내려오고, 밤이 시작됩니다.<br>마피아와 다른 역할들이 행동할 시간입니다.<br>투표 창에 지목할 사람의 이름을 입력하세요.</html>"); //밤이 되었음을 알리는 멘트 출력, 마피아 활동
+              displayRoleImage("src/mafia/image/마피아활동.png"); //마피아 이미지 출력
               break;
             case "police":
               clientGame.now = DayNight.POLICE;
-              clientGame.gp.ment.setText("<html>마피아의 정체를 파헤치기 위해 조사를 진행하고,<br>도시의 안전을 위해 헌신하는 경찰관입니다.<br>투표 창에 지목할 사람의 이름을 입력하세요.</html>");
-              //clientGame.gp.setBackground(Color.BLUE);
-              displayRoleImage("src/mafia/image/경찰활동.png");
+              clientGame.gp.ment.setText("<html>마피아의 정체를 파헤치기 위해 조사를 진행하고,<br>도시의 안전을 위해 헌신하는 경찰관입니다.<br>투표 창에 지목할 사람의 이름을 입력하세요.</html>"); //경찰 활동
+              displayRoleImage("src/mafia/image/경찰활동.png"); //경찰 이미지 출력
               break;
             case "heal":
               clientGame.now = DayNight.HEAL;
-              clientGame.gp.ment.setText("<html>마피아의 공격으로부터 플레이어들을 보호하고,<br>생존을 위해 노력하는 의지 강한 의사입니다.<br>투표 창에 지목할 사람의 이름을 입력하세요.</html>");
-              //clientGame.gp.setBackground(Color.RED);
-              displayRoleImage("src/mafia/image/의사활동.png");
+              clientGame.gp.ment.setText("<html>마피아의 공격으로부터 플레이어들을 보호하고,<br>생존을 위해 노력하는 의지 강한 의사입니다.<br>투표 창에 지목할 사람의 이름을 입력하세요.</html>"); //의사 활동
+              displayRoleImage("src/mafia/image/의사활동.png"); //의사 이미지 출력
               break;
           }
           clientGame.gp.chat.setEnabled(clientGame.now == DayNight.DAY || clientGame.isDead);
@@ -109,12 +105,12 @@ public class ClientGameChat extends Thread {
 
   }
 
-  private void displayRoleImage(String s) {
+  private void displayRoleImage(String s) { //오른쪽 하단에 현재 진행 상황을 알리는 이미지 출력하는 함수
     ImageIcon roleIcon = new ImageIcon(s);
     Image roleImage = roleIcon.getImage();
-    Image newRoleImage = roleImage.getScaledInstance(450, 300, Image.SCALE_SMOOTH);
+    Image newRoleImage = roleImage.getScaledInstance(450, 300, Image.SCALE_SMOOTH); //이미지 크기 설정
     roleIcon = new ImageIcon(newRoleImage);
 
-    clientGame.gp.roleImageLabel.setIcon(roleIcon);
+    clientGame.gp.roleImageLabel.setIcon(roleIcon); //roleImageLabel에 이미지 출력
   }
 }
--- src/mafia/GamePanel.java
+++ src/mafia/GamePanel.java
@@ -12,78 +12,84 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class GamePanel extends JPanel {
-  public JTextField chat;
-  public JScrollPane scrollPane;
-  public JTextArea chatArea;
-  public JButton startBtn;
-  public JTextField vote;
-  public JLabel member;
-  public JLabel time;
-  public JLabel ment; //진행 상황 멘트 표시
-  public JLabel roleImageLabel; //우측 하단에 역할 사진 표시
+  public JTextField chat; //채팅 입력하는 창
+  public JScrollPane scrollPane; //스크롤바 생성
+  public JTextArea chatArea; //채팅창
+  public JButton startBtn; //시작 버튼
+  public JTextField vote; //투표 입력하는 창
+  public JLabel member; //참여 인원
+  public JLabel time; //남은 시간
+  public JLabel ment; //진행 상황 멘트
+  public JLabel roleImageLabel; //우측 하단에 역할 사진
 
-  public GamePanel(JFrame frame, ClientGame clientGame) {
+  public GamePanel(JFrame frame, ClientGame clientGame) { //생성자 함수
     this.setLayout(null);
-    this.setBackground(new Color(24, 24, 40));
+    this.setBackground(new Color(24, 24, 40)); //배경색
 
+    //현재 진행 상황을 멘트로 알려주는 코드
     ment = new JLabel("<html>** 현재 진행 상황 **</html>");
     ment.setForeground(Color.WHITE);
     ment.setFont(new Font("굴림", Font.PLAIN, 15));
     ment.setBounds(800, 500, 500, 200);
-    this.add(ment);
 
+    //오른쪽 하단에 진행 상황을 알려주는 이미지
     roleImageLabel = new JLabel();
     roleImageLabel.setBounds(800, 650, 600, 200);
-    this.add(roleImageLabel);
 
+    //채팅
     JLabel chatable = new JLabel("채팅");
     chatable.setForeground(Color.WHITE);
     chatable.setFont(new Font("굴림", Font.PLAIN, 25));
     chatable.setBounds(20, 530, 73, 53);
 
+    //채팅 입력
     chat = new JTextField(30);
     chat.setBounds(90, 542, 694, 30);
     chat.addActionListener(new ActionListener() {
       @Override
-      public void actionPerformed(ActionEvent e) {
-        String s = chat.getText();
+      public void actionPerformed(ActionEvent e) { //사용자가 채팅 입력 후 엔터 키를 눌렀을 때 동작하는 리스너
+        String s = chat.getText(); //사용자가 입력한 채팅 메시지를 가져옴
 
-        if (clientGame.isDead) {
-          clientGame.sendMessage(clientGame.name + "/chat/" + s + "/death");
-          chatArea.append(clientGame.name + " (사망자) : " + s + "\n");
+        if (clientGame.isDead) { //플레이어가 사망했다면
+          clientGame.sendMessage(clientGame.name + "/chat/" + s + "/death"); //채팅 메시지와 플레이어의 사망 상태를 서버로 전송
+          chatArea.append(clientGame.name + " (사망자) : " + s + "\n"); //채팅 영역에 사망자로 표시 & 채팅 내용 추가
         } else {
-          clientGame.sendMessage(clientGame.name + "/chat/" + s + "/alive");
-          chatArea.append(clientGame.name + " (생존자) : " + s + "\n");
+          clientGame.sendMessage(clientGame.name + "/chat/" + s + "/alive"); //채팅 메시지와 플레이어의 생존 상태를 서버로 전송
+          chatArea.append(clientGame.name + " (생존자) : " + s + "\n"); //채팅 영역에 생존자로 표시 & 채팅 내용 추가
         }
-        chat.setText("");
-        chatArea.setCaretPosition(chatArea.getDocument().getLength());
+        chat.setText(""); //채팅 입력창을 초기화
+        chatArea.setCaretPosition(chatArea.getDocument().getLength()); //채팅 영역을 맨 아래로 스크롤
       }
     });
 
+    //채팅창
     chatArea = new JTextArea(10, 30);
     chatArea.setBackground(new Color(224, 224, 224));
     scrollPane = new JScrollPane(chatArea);
     scrollPane.setBounds(20, 39, 762, 493);
-    this.add(scrollPane);
+    this.add(scrollPane); //스크롤바 생성
     chatArea.setEditable(false);
 
+    //투표
     JLabel votelable = new JLabel("투표");
     votelable.setFont(new Font("굴림", Font.PLAIN, 25));
     votelable.setForeground(Color.WHITE);
     votelable.setBounds(20, 593, 73, 53);
 
+    //투표 입력
     vote = new JTextField();
     vote.setBounds(90, 608, 694, 30);
     vote.setColumns(10);
-    vote.addActionListener(new ActionListener() {
+
+    vote.addActionListener(new ActionListener() { //사용자가 투표 입력 후 엔터 키를 눌렀을 때 동작하는 리스너
       @Override
       public void actionPerformed(ActionEvent e) {
-        String s = vote.getText();
-        String[] strs = s.split("#");
+        String s = vote.getText(); //사용자가 입력한 투표 정보를 가져옴
+        String[] strs = s.split("#"); //이름#숫자 이런 형태이므로 이름과 숫자를 추출
         boolean nameCheck = false;
 
-        List<String> registeredNames = new ArrayList<>();
-        for(String name : clientGame.userNames){
+        List<String> registeredNames = new ArrayList<>(); //등록된 플레이어 이름을 확인하기 위한 리스트
+        for(String name : clientGame.userNames){ //등록된 플레이어들의 이름을 리스트에 추가하고, 동시에 입력된 이름이 올바른지 체크하는 코드
           registeredNames.add(name.split("#")[0]);
           if(strs.length>1){
             if(s.equals(name)){
@@ -96,6 +102,7 @@ public class GamePanel extends JPanel {
           }
         }
 
+        //이름이 중복된 경우 예외 처리
         int count = 0;
         for(String rn : registeredNames){
           if(strs.length<2){
@@ -109,27 +116,25 @@ public class GamePanel extends JPanel {
           }
         }
 
+        //올바른 이름이 입력되지 않았을 경우 오류창 띄우게 예외 처리
         if(!nameCheck){
           JOptionPane.showMessageDialog(frame, "정확한 이름을 입력하세요.\n ex)(이름) or (이름)#(숫자)", "오류", JOptionPane.ERROR_MESSAGE);
           return;
         }
 
-        if (clientGame.now == DayNight.NIGHT) {
-          //clientGame.gp.jlabel2.setText(clientGame.name + "은 마피아!!");
-          clientGame.sendMessage(clientGame.name + "/kill/" + s);
+        if (clientGame.now == DayNight.NIGHT) { //현재 상태가 밤이라면
+          clientGame.sendMessage(clientGame.name + "/kill/" + s); //죽음 투표
         } else if (clientGame.now == DayNight.HEAL) {
-          clientGame.sendMessage(clientGame.name + "/heal/" + s);
+          clientGame.sendMessage(clientGame.name + "/heal/" + s); //치료 투표
         } else if (clientGame.now == DayNight.POLICE) {
-          clientGame.sendMessage(clientGame.name + "/police/" + s);
+          clientGame.sendMessage(clientGame.name + "/police/" + s); //조사 투표
         } else {
-          clientGame.sendMessage(clientGame.name + "/vote/" + s);
+          clientGame.sendMessage(clientGame.name + "/vote/" + s); //낮인 경우 투표를 서버로 전송
         }
         vote.setText("");
         chatArea.setCaretPosition(chatArea.getDocument().getLength());
       }
     });
-
-    JLabel[] roleLabels = new JLabel[10];
 
     // 생존자 사진
     /*
@@ -138,24 +143,28 @@ public class GamePanel extends JPanel {
       this.add(roleLabels[i]);
     }
     */
+    JLabel[] roleLabels = new JLabel[10]; //10개 배열 생성
 
     //배열에 사진명 넣으면 해당 사진이 화면 출력됨
-    String[] roles = {"citizen","citizen","police"};
+    String[] roles = {"citizen","citizen","police"}; //시민 2명, 경찰 1명
     for (int i = 0; i < roles.length; i++) {
       roleLabels[i + 2] = createRoleLabel("src/mafia/image/" + roles[i] + ".png", 790 + i * 150, 39);
       this.add(roleLabels[i + 2]);
     }
 
-    String[] roles2 = {"mafia","mafia","doctor"};
+    String[] roles2 = {"mafia","mafia","doctor"}; //마피아 2명, 의사 1명
     for (int i = 0; i < roles2.length; i++) {
       roleLabels[i + 2] = createRoleLabel("src/mafia/image/" + roles2[i] + ".png", 790 + i* 150, 300);
       this.add(roleLabels[i + 2]);
     }
+
+    //참여 인원 정보
     member = new JLabel("참여인원");
     member.setForeground(Color.WHITE);
     member.setFont(new Font("굴림", Font.PLAIN, 20));
     member.setBounds(790, 230, 763, 85);
 
+    //시작 버튼
     startBtn = new JButton("시작");
     startBtn.setForeground(Color.WHITE);
     startBtn.setFont(new Font("굴림", Font.PLAIN, 15));
@@ -164,22 +173,25 @@ public class GamePanel extends JPanel {
     startBtn.setFocusPainted(false);
     startBtn.setBounds(20, 680, 80, 45);
 
+    /*
     ImageIcon startIcon = new ImageIcon("src/mafia/image/start.png");
     Image startImage = startIcon.getImage().getScaledInstance(80, 75, Image.SCALE_SMOOTH);
     startIcon = new ImageIcon(startImage);
     startBtn.setIcon(startIcon);
 
-    startBtn.addActionListener(new ActionListener() {
+     */
+
+    startBtn.addActionListener(new ActionListener() { //시작 버튼 클릭 시 이벤트 리스너
       @Override
       public void actionPerformed(ActionEvent e) {
         clientGame.sendMessage(clientGame.name + "/gameStart");
       }
     });
 
-    String[] processingTag = clientGame.name.split("#");
-    String tag = processingTag[processingTag.length - 1];
-    if (!tag.equals("1")) {
-      startBtn.setEnabled(false);
+    String[] processingTag = clientGame.name.split("#"); //플레이어 이름에서 #을 기준으로 분리하여 배열에 저장
+    String tag = processingTag[processingTag.length - 1]; //이름#1 마지막 요소를 태그로 사용
+    if (!tag.equals("1")) { //1이 아니면 -> 즉, 첫 번째 플레이어가 아니면
+      startBtn.setEnabled(false); //시작 버튼 비활성화
     }
 
 /*
@@ -213,7 +225,6 @@ public class GamePanel extends JPanel {
  */
 
     time = new JLabel("30");
-    //time.setForeground(Color.YELLOW); //노란색 적용이 안됨 개빡침
     time.setBounds(150, 680, 80, 45);
     time.setFont(new Font("굴림", Font.PLAIN, 30));
     time.setForeground(Color.WHITE);
@@ -228,11 +239,13 @@ public class GamePanel extends JPanel {
     this.add(member);
     this.add(vote);
     this.add(time);
+    this.add(ment);
+    this.add(roleImageLabel);
 
     setVisible(true);
   }
 
-  private String extractName(String fullName) {
+  private String extractName(String fullName) { //이름에서 #을 추출하는 함수
     String[] parts = fullName.split("#");
     return parts[0];
   }
